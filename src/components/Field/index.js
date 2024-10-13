274153fa83cb56b6181e94c8ae888bd01b5b5fe7
Julie AT <julie30at@gmail.com>
2024-10-13 19:15:22 +0200

    correction erreur formulaire

--- src/components/Field/index.js
+++ src/components/Field/index.js
@@ -17,12 +17,11 @@ const Field = ({ type = FIELD_TYPES.INPUT_TEXT, label, name, placeholder }) => {
           name={name}
           placeholder={placeholder}
           data-testid="field-testid"
-          required
         />
       );
       break;
     case FIELD_TYPES.TEXTAREA:
-      component = <textarea name={name} data-testid="field-testid" required />;
+      component = <textarea name={name} data-testid="field-testid" />;
       break;
     default:
       component = (
